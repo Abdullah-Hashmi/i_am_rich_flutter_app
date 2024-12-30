641a472132dc98250deb5321a7498c4715704d7a
Syed Abdullah <syedabdullah.developer@gmail.com>
2024-12-30 19:48:34 +0500

    Completed my first super easy flutter app
    🎉 Completed my first Flutter app - "I Am Rich"! 🚀
    Designed a sleek UI with gradients, shadows, and polished styling. Excited for more Flutter adventures! 🌟

--- lib/main.dart
+++ lib/main.dart
@@ -5,29 +5,73 @@ void main() {
     MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor:
-            Colors.blueGrey, // Set background color for the whole Scaffold
-        appBar: AppBar(
-          centerTitle: true,
-          title:
-              // white text
-              const Text(
-            "I am Rich",
-            style: TextStyle(color: Colors.white),
-          ), // Use const for unchanging Text widgets
-          backgroundColor: Colors.blueGrey[900],
-        ),
-        // Add your image here
-        body: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(20.0),
-              child: const Image(
-                image: AssetImage('images/diamond.png'),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Color(0xFF34495E),
+                Color(0xFF2C3E50),
+              ],
+            ),
+          ),
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20.0),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.5),
+                          offset: const Offset(4, 4),
+                          blurRadius: 10.0,
+                        ),
+                      ],
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(20.0),
+                      child: const Image(
+                        image: AssetImage('images/diamond.png'),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20), // Added more spacing for better layout
+                  const Text(
+                    "By: Syed Abdullah",
+                    style: TextStyle(
+                      color: Colors.white,
+                      shadows: [
+                        Shadow(
+                          color: Colors.black87,
+                          offset: Offset(2.0, 2.0),
+                          blurRadius: 4.0,
+                        ),
+                      ],
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 1.2, // Added letter spacing
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
+        ),
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text(
+            "I am Rich",
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          backgroundColor: Colors.blueGrey[900],
         ),
       ),
     ),
